4ae660ecbf6f7eb1c7d39a8f91a49c688c863142
SINOVIA <somayfield@gmail.com>
2016-05-31 17:10:35 -0400

    End of Day Push

---
+++ Modal/js/interaction.js
@@ -0,0 +1,9 @@
+"use strict";
+$( "#button").on("click", function() {
+  $(".theModal").addClass("showing")
+});
+
+"use strict";
+$( "#button2").on("click", function() {
+  $(".theModal").removeClass("showing")
+});
